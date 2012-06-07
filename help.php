8600c9c155927bfa7655abc840d3f641ac6a8430
gopiplus@hotmail.com <gopiplus@hotmail.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-07 05:06:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/anything-popup/trunk@554173 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,7 @@
+<h2>Plugin configuration option</h2>
+<ol>
+	<li>Drag and drop the widget</li>
+	<li>Paste the PHP code to your desired template location.</li>
+<!--	<li>Short code for pages and posts.</li>-->
+</ol>
+Check Official website, if any issue on popup positioning and more info <a href="http://www.gopiplus.com/work/2012/05/25/wordpress-popup-plugin-anything-popup/" target="_blank">click here</a>
